16a9793c544907f1cf0c7b899302fc525280f6b1
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2025-11-02 19:39:20 +0300

    aid

---
+++ alx_travel_app/alx_travel_app/listings/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views # Assuming you will create views in listings/views.py
+
+urlpatterns = [
+    # path('endpoint/', views.my_view, name='my_view_name'), # Add your API paths here later
+]
